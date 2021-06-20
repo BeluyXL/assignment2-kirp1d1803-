f9e4a242e210bc678906a38bcf1059d3b17c3205
BeluyXL <pit22062001@gmail.com>
2021-06-21 01:52:49 +0300

    added addItems method, checked with different values, works fin

--- src/Map.java
+++ src/Map.java
@@ -3,13 +3,19 @@ import java.util.Arrays;
 
 public class Map {
     char[][] chars;
+    int rows;
+    int cols;
     public Map (int rows, int cols){
         chars = new char[rows][cols];
+        this.rows = rows;
+        this.cols = cols;
         for (char[] aChar : chars) {
             Arrays.fill(aChar, '.');
         }
     }
     public Map(char[][] mapTemplate){
+        this.rows = mapTemplate.length;
+        this.cols = mapTemplate[0].length;
         chars = new char[mapTemplate.length][mapTemplate[0].length];
         chars = mapTemplate.clone();
     }
@@ -29,6 +35,25 @@ public class Map {
         Arrays.fill(chars[chars.length - 1], 'X');
 
 
+
+    }
+    public void addItems(int numOfItems){
+        char[] items = new char[numOfItems];
+        for (int i = 0; i < numOfItems; i++) {
+            items[i] = (char)(i+1 + '0');
+        }
+
+        int counter = 0;
+        int itemCounter = 0;
+        while (counter < numOfItems){
+            int r = GameData.randomRoll(0, rows);
+            int c = GameData.randomRoll(0, cols);
+            if(chars[r][c] != 'X'){
+                chars[r][c] = items[itemCounter];
+                counter++;
+                itemCounter++;
+            }
+        }
 
     }
 
