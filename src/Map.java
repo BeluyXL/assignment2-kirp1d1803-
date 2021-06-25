75755356888c23fbf89e6a7e40e3f9220bded3be
BeluyXL <pit22062001@gmail.com>
2021-06-26 00:44:48 +0300

    Map.java completed

--- src/Map.java
+++ src/Map.java
@@ -5,7 +5,11 @@ public class Map {
     char[][] chars;
     int rows;
     int cols;
-    public Map (int rows, int cols){
+    int heroRow = 0;
+    int heroCol = 0;
+    Hero hero = new Hero();
+
+    public Map(int rows, int cols) {
         chars = new char[rows][cols];
         this.rows = rows;
         this.cols = cols;
@@ -13,49 +17,168 @@ public class Map {
             Arrays.fill(aChar, '.');
         }
     }
-    public Map(char[][] mapTemplate){
+
+    public Map(char[][] mapTemplate) {
         this.rows = mapTemplate.length;
         this.cols = mapTemplate[0].length;
         chars = new char[mapTemplate.length][mapTemplate[0].length];
         chars = mapTemplate.clone();
     }
-    public String toString(){
+
+    public String toString() {
         for (int i = 0; i < chars.length; i++) {
             System.out.println(chars[i]);
         }
         return "";
 
     }
-    public void addWalls(){
+
+    public void addWalls() {
         for (int i = 0; i < chars.length; i++) {
             chars[i][0] = 'X';
-            chars[i][chars[i].length -1] = 'X';
+            chars[i][chars[i].length - 1] = 'X';
         }
         Arrays.fill(chars[0], 'X');
         Arrays.fill(chars[chars.length - 1], 'X');
 
 
-
     }
-    public void addItems(int numOfItems){
+
+    public void addItems(int numOfItems) {
         char[] items = new char[numOfItems];
         for (int i = 0; i < numOfItems; i++) {
-            items[i] = (char)(i+1 + '0');
+            items[i] = (char) (i + 1 + '0');
         }
-
         int counter = 0;
-        int itemCounter = 0;
-        while (counter < numOfItems){
+        while (counter < numOfItems) {
             int r = GameData.randomRoll(0, rows);
             int c = GameData.randomRoll(0, cols);
-            if(chars[r][c] != 'X'){
-                chars[r][c] = items[itemCounter];
+            if (chars[r][c] != 'X') {
+                chars[r][c] = items[counter];
                 counter++;
-                itemCounter++;
             }
         }
 
     }
 
+    public void initHeroLocation() {
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars[i].length; j++) {
+                if (chars[i][j] == 'H') {
+                    heroRow = i;
+                    heroCol = j;
+                }
+            }
+        }
+        if (heroRow == 0 && heroCol == 0) {
+            while (true) {
+                if (heroRow == 0 && heroCol == 0) {
+                    heroRow = GameData.randomRoll(0, rows);
+                    heroCol = GameData.randomRoll(0, cols);
+                    if (chars[heroRow][heroCol] != 'X') {
+                        chars[heroRow][heroCol] = 'H';
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    private boolean isValidMove(int row, int col) {
+        if (chars[row][col] != 'X') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private char applyMove(int endRow, int endCol) {
+        if (isValidMove(endRow, endCol)) {
+            chars[endRow][endCol] = 'H';
+            heroRow = endRow;
+            heroCol = endCol;
+        }
+        return chars[heroRow][heroCol];
+
+    }
+
+    public char processInput(int direction) {
+        if (direction == 0) {
+            if (isValidMove(heroRow - 1, heroCol)) {
+                if (chars[heroRow - 1][heroCol] != '.') {
+                    chars[heroRow - 1][heroCol] = '.';
+                    applyMove(heroRow - 1, heroCol);
+                    chars[heroRow + 1][heroCol] = '.';
+                    return chars[heroRow][heroCol];
+                } else {
+                    applyMove(heroRow - 1, heroCol);
+                    chars[heroRow + 1][heroCol] = '.';
+                    return chars[heroRow][heroCol];
+                }
+            } else {
+                return 'X';
+            }
+
+        }
+
+         else if (direction == 1) {
+            if (isValidMove(heroRow, heroCol + 1)) {
+                if (chars[heroRow][heroCol + 1] != '.') {
+                    chars[heroRow][heroCol + 1] = '.';
+                    applyMove(heroRow, heroCol + 1);
+                    chars[heroRow][heroCol - 1] = '.';
+                    return chars[heroRow][heroCol];
+                } else {
+                    applyMove(heroRow, heroCol + 1);
+                    chars[heroRow][heroCol - 1] = '.';
+                    return chars[heroRow][heroCol];
+                }
+            } else {
+                return 'X';
+            }
+
+        } else if (direction == 2) {
+            if (isValidMove(heroRow + 1, heroCol)) {
+                if (chars[heroRow + 1][heroCol] != '.') {
+                    chars[heroRow + 1][heroCol] = '.';
+                    applyMove(heroRow + 1, heroCol);
+                    chars[heroRow - 1][heroCol] = '.';
+                    return chars[heroRow][heroCol];
+                } else {
+                    applyMove(heroRow + 1, heroCol);
+                    chars[heroRow - 1][heroCol] = '.';
+                    return chars[heroRow][heroCol];
+                }
+            } else {
+                return 'X';
+            }
+
+        } else if (direction == 3) {
+            if (isValidMove(heroRow, heroCol - 1)) {
+                if (chars[heroRow][heroCol - 1] != '.') {
+                    chars[heroRow][heroCol - 1] = '.';
+                    applyMove(heroRow, heroCol - 1);
+                    chars[heroRow][heroCol + 1] = '.';
+                    return chars[heroRow][heroCol];
+                } else {
+                    applyMove(heroRow, heroCol - 1);
+                    chars[heroRow][heroCol + 1] = '.';
+                    return chars[heroRow][heroCol];
+                }
+            } else {
+                return 'X';
+            }
+        } else {
+            return 'X';
+        }
+    }
 }
+
+
+
+
+
+
+
+
